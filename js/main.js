31059f66f5685168052a6b8a7b4df39d742325de
Mai Trong Nhan <mainhan.nm30@gmail.com>
2021-12-02 15:38:21 +0700

    Add more code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 const $ = document.querySelector.bind(document),
-        $$ = document.querySelectorAll.bind(document);
-    
+    $$ = document.querySelectorAll.bind(document);
 
-const locationDropdown = $(".search .locations")
 
-locationDropdown.addEventListener('click', function() {
-    $('.locations__dropdown').style.display = 'block'
+const locations = $(".search__item.locations"),
+    dropDown = $('.locations__dropdown')
+
+
+const options = $('.nav__item.options'),
+    optionsDropdown = $('.options__list')
+
+options.addEventListener('click', function () {
+    optionsDropdown.classList.toggle('display-block')
 })
 
+const search = $('.search')
+
+let prevPos = window.pageYOffset
+window.onscroll = function () {
+    let currentPos = window.pageYOffset
+    if (prevPos > currentPos) {
+        search.style.top = '0'
+    } else {
+        search.style.top = '-100px'
+    }
+
+    prevPos = currentPos
+}
+
+const visitors = $('.visitor'),
+    visitorsList = $('.visitors__list')
+
+visitors.addEventListener('click', function () {
+    visitorsList.classList.toggle('display-block')
+})
+
+const searchItems = $$('.search__item'),
+    searchBtn = $('.btn__search')
+
+searchItems.forEach(searchItem => {
+    searchItem.addEventListener('click', function () {
+        searchBtn.classList.toggle('active')
+    })
+});
