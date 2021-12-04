48c506a82656f0527f8b1337044209e8818399fa
Mai Trong Nhan <mainhan.nm30@gmail.com>
2021-12-04 17:30:20 +0700

    Update code

--- js/main.js
+++ js/main.js
@@ -1,44 +1,78 @@
-const $ = document.querySelector.bind(document),
-    $$ = document.querySelectorAll.bind(document);
 
 
-const locations = $(".search__item.locations"),
-    dropDown = $('.locations__dropdown')
+// const $ = document.querySelector.bind(document),
+//     $$ = document.querySelectorAll.bind(document);
 
 
-const options = $('.nav__item.options'),
-    optionsDropdown = $('.options__list')
+// const locations = $(".search__item.locations"),
+//     dropDown = $('.locations__dropdown')
 
-options.addEventListener('click', function () {
-    optionsDropdown.classList.toggle('display-block')
-})
 
-const search = $('.search')
+// const options = $('.nav__item.options'),
+//     optionsDropdown = $('.options__list')
 
-let prevPos = window.pageYOffset
-window.onscroll = function () {
-    let currentPos = window.pageYOffset
-    if (prevPos > currentPos) {
-        search.style.top = '0'
-    } else {
-        search.style.top = '-100px'
-    }
+// options.addEventListener('click', function () {
+//     optionsDropdown.classList.toggle('display-block')
+// })
 
-    prevPos = currentPos
-}
+// const search = $('.search')
 
-const visitors = $('.visitor'),
-    visitorsList = $('.visitors__list')
+// let prevPos = window.pageYOffset
+// window.onscroll = function () {
+//     let currentPos = window.pageYOffset
+//     if (prevPos > currentPos) {
+//         search.style.top = '0'
+//     } else {
+//         search.style.top = '-100px'
+//     }
 
-visitors.addEventListener('click', function () {
-    visitorsList.classList.toggle('display-block')
-})
+//     prevPos = currentPos
+// }
 
-const searchItems = $$('.search__item'),
-    searchBtn = $('.btn__search')
+// const visitors = $('.visitor'),
+//     visitorsList = $('.visitors__list')
 
-searchItems.forEach(searchItem => {
-    searchItem.addEventListener('click', function () {
-        searchBtn.classList.toggle('active')
-    })
-});
+// visitors.addEventListener('click', function () {
+//     visitorsList.classList.toggle('display-block')
+// })
+
+// const searchItems = $$('.search__item'),
+//     searchBtn = $('.btn__search')
+
+// searchItems.forEach(searchItem => {
+//     searchItem.addEventListener('click', function () {
+//         searchBtn.classList.toggle('active')
+//     })
+// });
+
+// window.addEventListener('scroll', function() {
+//     console.log(this.scrollY)
+// })
+
+// import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+ const sliders = $$('.swiper-slide')
+
+ sliders.forEach(slider => {
+     if(slider.classList.contain('.swiper-slide-active'))
+        $('.slider__testimonial').style.display = 'block'
+ });
